361e2bcf5029d93eb07b84dea957d7c17ecb2d58
Ninho12 <joaopaulojpsp@gmail.com>
2023-01-07 16:25:18 -0300

    Editando a logica do jogo

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // variavel escolha
 var escolha = null;
 
+
+// Função que escolhe a opção
 function escolher(opcao){
 
     if(escolha != null){
@@ -10,5 +12,26 @@ function escolher(opcao){
 
     opcao.classList.add("escolha");
     escolha = opcao;
+}
+
+// Função que confirma a opção
+function confirmar(){
+
+    var opcao = "Windows";
+
+    if(escolha != null){
+        if(escolha.textContent == opcao){
+            alert("Você Acertou!");
+            escolha.classList.remove("escolha");
+            escolha.classList.add("acertou");
+        }else{
+            alert("Você Errou!");
+            escolha.classList.remove("escolha");
+            escolha.classList.add("errou");
+        }
+    }else{
+        alert("Escolha uma das opções");
+        
+    }
 
 }
